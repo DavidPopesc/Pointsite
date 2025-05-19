c540ba036c3f6c9dcf09f49d2cf48559c4998df9
David Popescu <davidpopescu511@yahoo.com>
2025-05-18 23:27:41 -0400

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -9,7 +9,6 @@ export default function Head() {
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="apple-mobile-web-app-title" content="Points" />
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      <p>test head text</p>
     </>
   );
 }
